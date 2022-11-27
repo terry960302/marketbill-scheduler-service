91dabdfe5d8bef34d7e2f02d66fdbe34533aba99
Taewan Kim <terry960302@gmail.com>
2022-11-27 21:44:18 +0900

    :recycle: change main file

--- flower-batch-upload-scheduler/main.go
+++ flower-batch-upload-scheduler/main.go
@@ -5,11 +5,11 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"public-flower-upload-scheduler/datastore"
 	"public-flower-upload-scheduler/handlers"
 	"public-flower-upload-scheduler/models"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func init() {
@@ -30,27 +30,27 @@ func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	}
 }
 
-// func main() {
-// 	lambda.Start(HandleRequest)
-// }
-
-// test
 func main() {
-	os.Setenv("PROFILE", "dev")
-	os.Setenv("DB_USER", "marketbill")
-	os.Setenv("DB_PW", "marketbill1234!")
-	os.Setenv("DB_NET", "tcp")
-	os.Setenv("DB_HOST", "marketbill-db.ciegftzvpg1l.ap-northeast-2.rds.amazonaws.com")
-	os.Setenv("DB_PORT", "5432")
-	os.Setenv("DB_NAME", "dev-db")
-	os.Setenv("PORT", "8080")
-	os.Setenv("API_KEY", "4DC6A10B4F5D43D5977F364FC0DFE81C")
-
-	db, _ := datastore.NewPostgresql()
-	db.AutoMigrate(&models.FlowerBatchUploadLogs{})
-
-	// flowers := []models.Flowers{}
-	// db.Table("flowers").Joins("FlowerTypes").Find(&flowers)
-
-	// utils.PrettyPrint(flowers)
+	lambda.Start(HandleRequest)
 }
+
+// // test
+// func main() {
+// 	os.Setenv("PROFILE", "dev")
+// 	os.Setenv("DB_USER", "marketbill")
+// 	os.Setenv("DB_PW", "marketbill1234!")
+// 	os.Setenv("DB_NET", "tcp")
+// 	os.Setenv("DB_HOST", "marketbill-db.ciegftzvpg1l.ap-northeast-2.rds.amazonaws.com")
+// 	os.Setenv("DB_PORT", "5432")
+// 	os.Setenv("DB_NAME", "dev-db")
+// 	os.Setenv("PORT", "8080")
+// 	os.Setenv("API_KEY", "4DC6A10B4F5D43D5977F364FC0DFE81C")
+
+// 	db, _ := datastore.NewPostgresql()
+// 	db.AutoMigrate(&models.FlowerBatchUploadLogs{})
+
+// 	// flowers := []models.Flowers{}
+// 	// db.Table("flowers").Joins("FlowerTypes").Find(&flowers)
+
+// 	// utils.PrettyPrint(flowers)
+// }
